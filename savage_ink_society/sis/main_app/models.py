fcc96cc210280e70155a5df34943c01086e7e6bc
dean0machin0 <dean0machin00@gmail.com>
2024-01-16 06:37:42 -0800

    Profile model further defined.

--- savage_ink_society/sis/main_app/models.py
+++ savage_ink_society/sis/main_app/models.py
@@ -4,4 +4,7 @@ from django.contrib.auth.models import User
 # Profile Model
 class Profile(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
-    userbio = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_img = models.ImageField(upload_to='profile_imgs/', blank=True)
+    location = models.CharField(max_length=50, blank=True)
+    birth_date = models.DateField(null=True, blank=True)

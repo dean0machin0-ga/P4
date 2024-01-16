0a96efbc22991922d9485f128e8d4f2025e7ce15
dean0machin0 <dean0machin00@gmail.com>
2024-01-16 04:33:46 -0800

    SIS about url, view function, and template defined.

--- savage_ink_society/sis/main_app/views.py
+++ savage_ink_society/sis/main_app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# Splash Page
+# Splash View
 def splash(request):
     return render(request, 'splash.html')
+
+# About View
+def about(request):
+    return render(request, 'about.html')

34e0a5e60aaf82d0c5aaf2837b47ff853324f0ed
dean0machin0 <dean0machin00@gmail.com>
2024-01-18 02:05:09 -0800

    Profile details succesfully rendered.

--- savage_ink_society/sis/main_app/models.py
+++ savage_ink_society/sis/main_app/models.py
@@ -20,11 +20,13 @@ class Profile(AbstractUser):
     def get_absolute_url(self):
         return reverse('detail')
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_or_update_profile(sender, instance, created, **kwargs):
+#     if created:
+#         if not hasattr(instance, 'profile'):
+#             Profile.objects.create(user=instance)
+#     else:
+#         instance.profile.save()
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def save_user_profile(sender, instance, **kwargs):
-    instance.save()
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def save_user_profile(sender, instance, **kwargs):

2fe15500bf12676bebf8ab79bd4f5a05a7c841cc
dean0machin0 <dean0machin00@gmail.com>
2024-01-18 08:21:22 -0800

    Views rendered for tatto img and comments.

--- savage_ink_society/sis/main_app/views.py
+++ savage_ink_society/sis/main_app/views.py
@@ -1,7 +1,8 @@
 # main_app/views 
 from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView, DeleteView
-from .models import Profile
+from .models import Profile, TattooImg, Comment
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -13,6 +14,13 @@ def splash(request):
 def about(request):
     return render(request, 'about.html')
 
+# Profile Create
+class ProfileCreate(CreateView):
+    model = Profile
+    fields = ['profile_img', 'bio', 'location', 'birth_date', 'astrological_sign']
+    template_name = 'profiles/profile_form.html'  # Specify the template name
+    success_url = reverse_lazy('profile_list')
+
 # Profile list 
 def profile_list(request):
     profiles = Profile.objects.all()
@@ -23,11 +31,11 @@ def profile_list(request):
 # Profile Detail
 def profile_details(request, profile_id):
     profile = Profile.objects.get(id=profile_id)
+    tattoo_imgs = TattooImg.objects.filter(comments__username=profile.username)
+    comments = Comment.objects.filter(username=profile.username)
+    
     return render(request, 'profiles/details.html', {
-        'profile': profile
+        'profile': profile,
+        'tattoo_imgs': tattoo_imgs,
+        'comments': comments,
     })
-
-# Profile Create View
-# class ProfileCreate(CreateView):
-#     model = Profile
-#     fields = __all__

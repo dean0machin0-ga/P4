068a26191ecdf8ef038bd168f096d12fcf0b2733
dean0machin0 <dean0machin00@gmail.com>
2024-01-18 05:47:57 -0800

    Comments and TattoImg models defined.

--- savage_ink_society/sis/main_app/models.py
+++ savage_ink_society/sis/main_app/models.py
@@ -1,10 +1,10 @@
 # Models
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.urls import reverse
-from django.conf import settings
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.conf import settings
 
 # Custom Profile Model
 class Profile(AbstractUser):
@@ -19,6 +19,39 @@ class Profile(AbstractUser):
 
     def get_absolute_url(self):
         return reverse('detail')
+
+# Comment Model
+class Comment(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    username = models.CharField(max_length=50)
+    content = models.TextField(max_length=750, blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.username})'
+
+# Tattoo Image Model
+class TattooImg(models.Model):
+    img = models.ImageField(upload_to="imgs", blank=False)
+    like_dislike = models.BooleanField(default=False)
+    comments = models.ManyToManyField(Comment, related_name='tattoo_comments')
+
+    def __str__(self):
+        return f'{self.img.name} (Likes: {self.like_dislike})'
+
+    def toggle_like_dislike(self):
+        self.like_dislike = not self.like_dislike
+        self.save()
+
+    def add_comment(self, comment):
+        self.comments += f"\n{comment}"
+        self.save()
+
+
+
+
+
+# Code In Limbo
 
 # @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 # def create_or_update_profile(sender, instance, created, **kwargs):

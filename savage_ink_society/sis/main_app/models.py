0566b7a7afd9d70e28c7a6f7759d2b4ae0cf984e
dean0machin0 <dean0machin00@gmail.com>
2024-01-23 04:26:43 -0800

    Bootstrap CSS implemented/README update

--- savage_ink_society/sis/main_app/models.py
+++ savage_ink_society/sis/main_app/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 # User Model
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True)
+    bio = models.TextField(max_length=250, blank=True)
     location = models.CharField(max_length=255, blank=True)
     astrological_sign = models.CharField(max_length=50, blank=True)
 
@@ -46,39 +46,3 @@ class TattooShop(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     business_page_link = models.URLField()
     photo = models.URLField()
-
-
-# Custom Profile Model
-# class Profile(AbstractUser):
-#     profile_img = models.ImageField(upload_to='profile_imgs/', blank=True)
-#     bio = models.TextField(max_length=500, blank=True)
-#     location = models.CharField(max_length=50, blank=True)
-#     birth_date = models.DateField(null=True, blank=True)
-#     astrological_sign = models.CharField(max_length=50, blank=True)
-
-#     def __str__(self):
-#         return f'{self.username} ({self.id})'
-
-#     def get_absolute_url(self):
-#         return reverse('details')
-    
-# Tattoo Image Model
-# class TattooImg(models.Model):
-#     img = models.ImageField(upload_to="tattoo_imgs", blank=False)
-#     like_dislike = models.BooleanField(default=False)
-#     comments = models.ManyToManyField(Comment, related_name='tattoo_comments')
-#     profile = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=1)
-#     # upload_date = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return f'{self.img.name} (Likes: {self.like_dislike})'
-
-#     def toggle_like_dislike(self):
-#         self.like_dislike = not self.like_dislike
-#         self.save()
-
-#     def add_comment(self, comment_content, username):
-#         comment_profile = get_user_model().objects.get(username=username).profile
-#         comment = Comment.objects.create(title='', content=comment_content, username=username, profile=comment_profile)
-#         self.comments.add(comment)
-#         self.save()

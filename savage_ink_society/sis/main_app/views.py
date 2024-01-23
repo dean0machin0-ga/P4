72f3c5ed683fda725242cc069db3786b192e8706
dean0machin0 <dean0machin00@gmail.com>
2024-01-23 01:33:24 -0800

    Bootstrap CSS installed and lightly implemented

--- savage_ink_society/sis/main_app/views.py
+++ savage_ink_society/sis/main_app/views.py
@@ -1,16 +1,15 @@
-# main_app/views 
+# main_app/views.py
 import os
 import uuid
 import boto3
 import requests
+import random
 from django.apps import AppConfig
-from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import ProfileForm, CommentForm
-from .models import CustomUser, Photo, TattooShop, Comment, BackgroundImage, Comment
-# from django.conf import settings
+from .models import CustomUser, Photo, Comment, BackgroundImage, Comment
 
 # Create your views here.
 
@@ -24,11 +23,7 @@ class MainAppConfig(AppConfig):
 # Splash View
 def splash(request):
     background_images = BackgroundImage.objects.all()
-    if background_images:
-        background_image = random.choice(background_images)
-    else:
-        background_image = None
-
+    background_image = random.choice(background_images) if background_images else None
     return render(request, 'splash.html', {'background_image': background_image})
 
 # About View
@@ -41,27 +36,19 @@ def about(request):
 class ProfileCreate(CreateView):
     model = CustomUser
     form_class = ProfileForm
-    template_name = 'profiles/profile_form.html' 
+    template_name = 'profiles/profile_form.html'
     success_url = reverse_lazy('profile_list')
 
 # Profile list 
 def profile_list(request):
     profiles = CustomUser.objects.all()
-    return render(request, 'profiles/list.html', {
-        'profiles': profiles
-    })
+    return render(request, 'profiles/list.html', {'profiles': profiles})
     
 # Profile Detail
 def profile_details(request, profile_id):
-    profile = CustomUser.objects.get(id=profile_id)
-    # tattoo_imgs = TattooImg.objects.filter(comments__username=profile.username)
+    profile = get_object_or_404(CustomUser, id=profile_id)
     comments = Comment.objects.filter(username=profile.username)
-    
-    return render(request, 'profiles/details.html', {
-        'profile': profile,
-        # 'tattoo_imgs': tattoo_imgs,
-        'comments': comments,
-    })
+    return render(request, 'profiles/details.html', {'profile': profile, 'comments': comments})
 
 # Add Photo View
 def add_photo(request, profile_id):
@@ -84,30 +71,26 @@ def add_photo(request, profile_id):
 # Comment List
 def comment_list(request):
     comments = Comment.objects.all()
-    return render(request, 'comments/list.html', {
-        'comments': comments
-    })
+    return render(request, 'comments/list.html', {'comments': comments})
 
 # Comment Detail
 def comment_detail(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
-    return render(request, 'comments/comment_details.html', {
-        'comment': comment,
-    })
+    return render(request, 'comments/comment_details.html', {'comment': comment})
 
 # Comment Create
 class CommentCreate(CreateView):
     model = Comment
     form_class = CommentForm
-    template_name = 'comments/comment_form.html' 
+    template_name = 'comments/comment_form.html'
     success_url = reverse_lazy('comment_list')
 
 # Comment Update
 class CommentUpdate(UpdateView):
     model = Comment
     fields = ['title', 'content']
-    template_name = 'comments/comment_form.html' 
-    
+    template_name = 'comments/comment_form.html'
+
 # Comment Delete
 class CommentDelete(DeleteView):
     model = Comment
@@ -119,10 +102,7 @@ class CommentDelete(DeleteView):
 # Tattoo Img List
 def tattoo_list(request):
     tattoos = TattooImg.objects.all()
-    return render(request, 'tattoo_imgs/list.html', {
-        'tattoos' : tattoos,
-    })
-
+    return render(request, 'tattoo_imgs/list.html', {'tattoos': tattoos})
 
 # SEARCH VIEWS
 
@@ -145,8 +125,6 @@ def search_results(request):
         }
         response = requests.get(url, headers=headers, params=params)
 
-        print(response.json())
-
         api_data = response.json().get('data', [])
 
         context = {
@@ -157,31 +135,3 @@ def search_results(request):
         return render(request, 'search_results.html', context)
     else:
         return render(request, 'splash.html')
-    
-
-        # for shop_data in api_data:
-        #     TattooShop.objects.create(
-        #         name=shop_data.get('name', ''),
-        #         rating=shop_data.get('rating', 0.0),
-        #         review_count=shop_data.get('review_count', 0),
-        #         price_range=shop_data.get('price', ''),
-        #         address=shop_data.get('location', {}).get('address1', ''),
-        #         phone=shop_data.get('phone', ''),
-        #         business_page_link=shop_data.get('url', ''),
-        #         photo=shop_data.get('image_url', '')
-        #     )
-
-        # tattoo_shops = TattooShop.objects.all()
-# Tattoo Details
-# class TattooImgDetail(DetailView):
-#     model = TattooImg
-#     template_name = 'tattoo_img/tattoo_img_details.html'
-#     context_object_name = 'tattoo_img'
-
-#  Tattoo Toggle
-# def toggle_like_dislike(request, tattoo_img_id):
-#     tattoo_img = get_object_or_404(TattooImg, id=tattoo_img_id)
-#     tattoo_img.like_dislike = not tattoo_img.like_dislike
-#     tattoo_img.save()
-
-#     return JsonResponse({'success': True})
